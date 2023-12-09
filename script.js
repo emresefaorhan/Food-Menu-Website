efd4fadcefd2cb9d9e634fbeef3e8db5a61f0059
emresefaorhan <emre.sf16@gmail.com>
2023-12-08 21:25:24 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const searchForm = document.querySelector(".search-form");
+const cartItem = document.querySelector(".cart-items-container");
+const navbar = document.querySelector(".navbar");
+
+//! buttons
+const searchBtn = document.querySelector("#search-btn");
+const cartBtn = document.querySelector("#cart-btn");
+const menuBtn = document.querySelector("#menu-btn");
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(searchBtn) &&
+      !e.composedPath().includes(searchForm)
+    ) {
+      searchForm.classList.remove("active");
+    }
+  });
+});
+
+cartBtn.addEventListener("click", function () {
+  cartItem.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(cartBtn) &&
+      !e.composedPath().includes(cartItem)
+    ) {
+      cartItem.classList.remove("active");
+    }
+  });
+});
+
+menuBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(menuBtn) &&
+      !e.composedPath().includes(navbar)
+    ) {
+      navbar.classList.remove("active");
+    }
+  });
+});
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
